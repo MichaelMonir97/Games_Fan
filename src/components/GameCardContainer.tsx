ffa98ec5fa1d02aef2916cc2b8b6b86834e2db51
MichaelMonir97 <fast_nemo@yahoo.com>
2023-06-27 20:49:07 +0300

    adding cards shadow and theme colors

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,11 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box overflow="hidden" borderRadius={15}>
+    <Box
+      overflow="hidden"
+      borderRadius={15}
+      boxShadow="0px 0px 5px 5px rgba(0, 0, 0, 0.1)"
+    >
       {children}
     </Box>
   );
